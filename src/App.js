7e04ce19bb8fe976846ba93e9fbf02d5617fefdd
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-05-15 11:23:12 +0100

    chore: created the chatbot

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 function App() {
   return (
     <div className="container" >
       <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles/Home.css'
+import ChatBot from '../components/ChatBot'
+const Home = () => {
+  return (
+    <div className='Home'>
+        <ChatBot />
+
+    </div>
+  )
+}
+
+export default Home
